a4c47ed6d81c48987c4a56b7f91b4706b5106e0e
Nikhil Kumar <kumarnikhil302021@gmail.com>
2026-05-14 14:41:26 +0530

    library-app done

---
+++ Library/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div id="parent">
+      <h1 id="child1">This is for the testing purpose.</h1>
+      <h2 id="child2">This is heading number 2.</h2>
+    </div>
+  );
+};
+
+export default App;
